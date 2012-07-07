ab71f002082949c905cc4decbd709efbc7353a40
Chris Carter <chris@tankjetstudios.com>
2012-07-07 17:41:51 -0400

    created app

---
+++ vangaurde/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vangaurde::Application.config.secret_token = 'f0d02d10d441146422ee683e6939941feb2555c8cd55da6288c78427c220b0a588261b95f9a852e197fc8010e69b31359e0ea48cb6bdb07302bdbba930e619ee'
---
+++ vangaurde/config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Vangaurde::Application.config.session_store :cookie_store, key: '_vangaurde_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Vangaurde::Application.config.session_store :active_record_store
