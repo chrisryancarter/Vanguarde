95b82aa0bab325f1b6e9068ffc85a4faa250a128
Chris Carter <chris@tankjetstudios.com>
2012-07-19 00:07:34 -0400

    added in the news page

--- db/schema.rb
+++ db/schema.rb
@@ -46,6 +46,16 @@ ActiveRecord::Schema.define(:version => 20120717170919) do
   add_index "admin_users", ["email"], :name => "index_admin_users_on_email", :unique => true
   add_index "admin_users", ["reset_password_token"], :name => "index_admin_users_on_reset_password_token", :unique => true
 
+  create_table "news", :force => true do |t|
+    t.string   "video"
+    t.string   "featured_image"
+    t.string   "title"
+    t.text     "subtext"
+    t.text     "fulltext"
+    t.datetime "created_at",     :null => false
+    t.datetime "updated_at",     :null => false
+  end
+
   create_table "news_images", :force => true do |t|
     t.string   "image"
     t.integer  "post_id"
