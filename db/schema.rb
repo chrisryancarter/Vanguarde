6e7f980b59668fce10ba0f1b91d53d76822e4885
Chris Carter <chris@tankjetstudios.com>
2012-07-18 22:24:07 -0400

    got stuff fonts working

--- db/schema.rb
+++ db/schema.rb
@@ -46,6 +46,16 @@ ActiveRecord::Schema.define(:version => 20120713033503) do
   add_index "admin_users", ["email"], :name => "index_admin_users_on_email", :unique => true
   add_index "admin_users", ["reset_password_token"], :name => "index_admin_users_on_reset_password_token", :unique => true
 
+  create_table "news", :force => true do |t|
+    t.string   "video"
+    t.string   "featured_image"
+    t.string   "title"
+    t.text     "subtext"
+    t.text     "fulltext"
+    t.datetime "created_at",     :null => false
+    t.datetime "updated_at",     :null => false
+  end
+
   create_table "posts", :force => true do |t|
     t.string   "title"
     t.text     "snippet"
