d11f8ba8af826cd5e03e93e5d7300ebed2f835bc
Matt Steedman <matt@tankjetstudios.com>
2012-07-30 12:19:36 -0400

    precompiles activea dmin assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,7 @@ Vangaurde::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  # Active admine
+  config.assets.precompile += %w[active_admin.css active_admin.js]
 end
