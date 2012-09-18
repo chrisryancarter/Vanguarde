729cd11dc3b0cf55584fd91451f5f5de84942d63
Chris Carter <chris@tankjetstudios.com>
2012-09-18 16:34:48 -0400

    worked on carousels

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -12,18 +12,62 @@ jQuery(function( $ ) {
     orientation: "vertical",
   });
 
-  $( "#carousel2" ).rcarousel({
-    auto: {
-      enabled: true,
-      interval: 3000,
-      direction: "prev"
-    },
-      width: 1058,
-      height: 794,
-      visible: 1,
-      step: 1,
-      orientation: "vertical",
- 
-  });
+function generatePages() {
+          var _total, i, _link;
+          
+          _total = $( "#carousel" ).rcarousel( "getTotalPages" );
+          
+          for ( i = 0; i < _total; i++ ) {
+            _link = $( "<a href='#'></a>" );
+            
+            $(_link)
+              .bind("click", {page: i},
+                function( event ) {
+                  $( "#carousel" ).rcarousel( "goToPage", event.data.page );
+                  event.preventDefault();
+                }
+              )
+              .addClass( "bullet off" )
+              .appendTo( "#pages" );
+          }
+          
+          // mark first page as active
+          $( "a:eq(0)", "#pages" )
+            .removeClass( "off" )
+            .addClass( "on" )
+            .css( "background-image", "url(../images/work/page-on.png)" );
+
+        }
+
+        function pageLoaded( event, data ) {
+          $( "a.on", "#pages" )
+            .removeClass( "on" )
+            .css( "background-image", "url(../images/work/page-off.png)" );
+
+          $( "a", "#pages" )
+            .eq( data.page )
+            .addClass( "on" )
+            .css( "background-image", "url(../images/work/page-on.png)" );
+        }       
+
+        $( "#carousel" ).rcarousel({
+          auto: {enabled: true},
+          start: generatePages,
+          visible: 1,
+          step: 1,
+          width: 1058,
+          height: 794,
+          pageLoaded: pageLoaded
+        });
+        
+        $( ".bullet" )
+          .hover(
+            function() {
+              $( this ).css( "opacity", 0.7 );
+            },
+            function() {
+              $( this ).css( "opacity", 1.0 );
+            }
+          );          
   		
 });
